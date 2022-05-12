fb6dd3bbe6e0cc195cc91ebc4184bc759fc7565f
kjh <aam41@naver.com>
2022-05-12 19:04:52 +0900

    test

--- src/main/java/com/kjh/hellospring/service/MemberService.java
+++ src/main/java/com/kjh/hellospring/service/MemberService.java
@@ -3,6 +3,8 @@ package com.kjh.hellospring.service;
 import com.kjh.hellospring.domain.Member;
 import com.kjh.hellospring.repository.MemoryMemberRepository;
 
+import javax.swing.text.html.Option;
+import java.util.List;
 import java.util.Optional;
 
 public class MemberService {
@@ -10,10 +12,22 @@ public class MemberService {
     private final MemoryMemberRepository memberRepository = new MemoryMemberRepository();
 
     public Long join(Member member){
-        Optional<Member> result =  memberRepository.findByName(member.getName());
-        result.ifPresent(m -> {
-            throw new IllegalAccessException("존재하는 회원입니다.");
-        });
+        validateDuplicateMember(member);
         return member.getId();
+    }
+
+    private void validateDuplicateMember(Member member) {
+        memberRepository.findByName(member.getName()).
+                ifPresent(m -> {
+            throw new IllegalStateException("존재하는 회원입니다.");
+        });
+    }
+
+    public List<Member> findMembers() {
+        return memberRepository.findAll();
+    }
+
+    public Optional<Member> findOne(Long memberId){
+        return memberRepository.findById(memberId);
     }
 }
